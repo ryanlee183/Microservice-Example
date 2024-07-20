41f01ca4dfbea80c1ecd9ccaaea2f11612d0664c
ryanlee183 <ryanlee183@hotmail.com>
2024-07-20 22:43:04 +0800

    Initial commit

---
+++ user-service/user-service/src/main/java/com/example/user/entity/User.java
@@ -0,0 +1,22 @@
+package com.example.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long departmentId;
+}
